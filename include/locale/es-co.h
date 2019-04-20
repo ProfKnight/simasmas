c9a2b55ec0d9e01f62ce2be12bd1533a6b034ed7
profknight <ahknight@pipeline.sbcc.edu>
2019-04-20 10:58:38 -0700

    Initial commit of first files

---
+++ include/locale/es-co.h
@@ -0,0 +1,5 @@
+#define para for
+#define mientras while
+#define si if
+#define entrega return
+#define primera main
---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "locale/es-co.h"
+
+void foo() {
+    return;
+}
+
+int primera() {
+    std::cout << "Hola, Mundo!" << std::endl;
+
+
+    para (int i = 0; i < 10; i++) {
+        si (i % 2 == 0) {
+            std::cout << i << std::endl;
+        }
+    }
+
+    int x = 10;
+    mientras(x > 0) {
+        x--;
+    }
+
+    entrega 0;
+}
